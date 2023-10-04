3d5d85bfed67330ce80ffcb557154c910748a2e9
thresh <u1395168@utah.edu>
2023-10-04 09:13:32 +0800

    finished spinner.py1

--- Spinner.py
+++ Spinner.py
@@ -13,3 +13,24 @@ class Spinner:
                 withoutspace = value.split(',')
                 synonyms[key] = withoutspace
         return synonyms
+
+    def random_spinner(self, text, Change_word_probability=50):
+        words = text.split()
+        spun_element = []
+
+        for word in words:
+            if word in self.synonym_dict:
+                if random.randint(1, 100) <= Change_word_probability:
+                    synonym_list = self.synonym_dict[word]
+                    substitutes = random.choice(synonym_list)
+                    spun_element.append(substitutes)
+                else:
+                    spun_element.append(word)
+            else:
+                spun_element.append(word)
+
+        spun_text = ""
+        for word in spun_element:
+            spun_text += word + " "
+
+        return spun_text
